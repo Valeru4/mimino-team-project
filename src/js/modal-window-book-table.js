4cd0b04d28cfa577b4e4a53c54a473d65687b54b
karanukvova <volodimirkaranuk@gmail.com>
2023-02-20 18:22:37 +0200

    connect modal window book table

--- src/js/modal-window-book-table.js
+++ src/js/modal-window-book-table.js
@@ -1,14 +1,32 @@
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+const modalt = document.getElementById('simpleModalt');
+// All page modals
+var modalst = document.querySelectorAll('.modalt');
+// Get open modal button
+const modalBtnt = document.querySelectorAll('.modal-btnt');
+// Get close button
+const closeBtnt = document.getElementsByClassName('closeBtnt')[0];
+
+// Listen 	for OPEN Click
+modalBtnt.forEach(function(e) {
+e.addEventListener('click', openModal);
+})
+// Listen for CLOSE Click
+closeBtnt.addEventListener('click', closeModal);
+// Listen for OUTSIDE Click
+window.addEventListener('click', outsideClick);
+
+// Function to OPEN modal
+function openModal() {
+  modalt.style.display = "block";
+}
+
+// Function to CLOSE modal
+function closeModal() {
+  modalt.style.display = "none";
+}
+// Function to CLOSE modal
+function outsideClick(e) {
+  if(e.target == modalt) {
+    modalt.style.display = "none";
+  }
+}
